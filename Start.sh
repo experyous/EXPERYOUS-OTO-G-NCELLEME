a3a5c96be99d1ee8d170204f378bd5700de4cdaf
experyous <71965927+experyous@users.noreply.github.com>
2021-09-02 22:48:01 +0300

    Add files via upload

---
+++ Start.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#Code: Experyous
+#Telegram: @experyous
+#Youtube : Experyous
+echo -e '\033[31;40;1m 
+		   ─────█─▄▀█──█▀▄─█─────
+		   ────▐▌──────────▐▌────
+		   ────█▌▀▄──▄▄──▄▀▐█────
+		   ───▐██──▀▀──▀▀──██▌───
+		   ──▄████▄──▐▌──▄████▄──
+      \e[35m      ☠         Code  : Experyous     ☠
+	  \e[34m☠	     Github : Experyous       ☠
+	 \e[33m☠  	    Youtube : Experyous        ☠
+	\e[32m☠ 	   Telegram : @Experyous      ☠
+	
+	\e[36m 	 BY EXPERYOUS
+\e[32m[1]\e[33mTermux Güncelle'
+
+read -p "İşlem Numarası: " islem
+if [[ $islem == 1 || $islem == 01 ]]; then
+pkg install upgrade
+pkg install update
+apt install git
+gem install lolcat
+pkg install python
+pkg install pyhon2
+pkg install python3
+pkg install nano
+pkg install git
+apt install git
+pkg install git -y
+pkg install python python2 -y
+pkg install wget -y
+pkg install curl -y
+pkg install ruby -y
+pkg install php -y
+pkg install wget
+pkg install php
+pkg install pip
+pkg install pip pip2 -y
+pkg install clang -y
+pkg install vim -y
+pkg install nano -y
+apt install proot -y
+pkg install cat -y
+pkg install figlet -y
+pkg install cmatrix -y
+pkg install perl -y
+apt update
+apt upgrade -y
+clear
+echo -e '\033[31;40;1m              Kurulum Tamamlandı ✅'
+sleep 2
+bash start.sh
