b5d7dae30e505ec457f861af1b4534a0c44f46f9
experyous <71965927+experyous@users.noreply.github.com>
2021-09-03 13:57:23 +0300

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 pkg install git -y
+apt update && apt upgrade -y
+	pkg install git -y
+	pkg install php -y
+	pkg install perl -y
+	pkg install nano -y
+	pkg install python python2 -y
+	pkg install pip pip2 -y
+	pip install --upgrade pip
+	pip install bs4 
+	pip install future 
+	pip install requests 
+	pip install colorama
+	pip install wordlist
+	pkg install termux-api -y
+	pkg install curl  -y
+	pkg install wget -y
+	pkg install neofetch -y
+	apt update && apt upgrade -y
 pkg install python python2 -y
 pkg install wget -y
 pkg install curl -y
